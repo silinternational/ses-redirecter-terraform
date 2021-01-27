a27875e7fd913a701046b5ce8db10bb38968620b
SBagwell <steve_bagwell@sil.org>
2021-01-27 16:22:36 -0500

    Don't need route53 info anymore

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,6 @@ output "function_arn" {
   value = aws_lambda_function.ses_redirecter.arn
 }
 
-output "record_mx_record" {
-  value = join(" ", [aws_route53_record.mx.name, aws_route53_record.mx.records[0]])
-}
-
-
 output "instructions" {
   value = data.template_file.instructions.rendered
 }
